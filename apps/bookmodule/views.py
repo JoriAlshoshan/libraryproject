f78a05cb563ac30a9f2e32f279965219be1880ed
Jori Alshoshan <jori.alshoshan@gmail.com>
2026-02-17 19:29:56 +0300

    Lab 4

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -19,3 +19,15 @@ def viewbook(request, bookId):
         targetBook = book2
 
     return render(request, 'bookmodule/show.html', {'book': targetBook})
+def index(request):
+    return render(request, "bookmodule/index.html")
+ 
+def list_books(request):
+    return render(request, 'bookmodule/list_books.html')
+ 
+def viewbook(request, bookId):
+    return render(request, 'bookmodule/one_book.html')
+ 
+def aboutus(request):
+    return render(request, 'bookmodule/aboutus.html')
+ 
